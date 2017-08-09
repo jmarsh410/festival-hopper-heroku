b07cbe306b9bfc77e20ca94734d51d69f04c6ddd
Jered Marshall <jmarsh410@gmail.com>
2017-08-08 19:49:34 -0500

    refined the webpack config for client bundles

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -1,31 +1,78 @@
 var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var ManifestPlugin = require('webpack-manifest-plugin');
+
+// Specify any environment variables?
+var env = {};
 
 module.exports = {
   watch: true,
-  entry: './src',
+  // We generate sourcemaps in production. This is slow but gives good results.
+  // You can exclude the *.map files from the build during deployment.
+  devtool: 'source-map',
+  entry: './src/index.js', // TODO: see if you want to add polyfills to the bundle
   output: {
-    filename: 'bundle.js',
+    // put the files in the dist-client folder
     path: path.resolve(__dirname, 'dist-client'),
+    // put the js bundle in static/js with a name and chunk hash
+    filename: 'static/js/[name].js',
+    chunkFilename: 'static/js/[name].chunk.js',
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'css-loader'
-        ]
+        use: ExtractTextPlugin.extract({
+          use: "css-loader"
+        })
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'static/media/[name].[ext]'
+          },
+        }
       },
       {
         test: /\.js/,
-        exclude: /node_modules/,
         loader: 'babel-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    // MANY OF THESE PLUGINS COPIED DIRECTLY FROM CREATE REACT APPS CONFIG
+
+    // Makes some environment variables available to the JS code, for example:
+    // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
+    // It is absolutely essential that NODE_ENV was set to production here.
+    // Otherwise React will be compiled in the very slow development mode.
+    // new webpack.DefinePlugin(env),
+
+    // This helps ensure the builds are consistent if source hasn't changed:
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    // Minify the code.
+    new ExtractTextPlugin("static/css/styles.css"),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        screw_ie8: true, // React doesn't support IE8
+        warnings: false
+      },
+      mangle: {
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+        screw_ie8: true
+      }
+    }),
+    // Generate a manifest file which contains a mapping of all asset filenames
+    // to their corresponding output file so that tools can pick it up without
+    // having to parse `index.html`.
+    new ManifestPlugin({
+      fileName: 'asset-manifest.json'
+    }),
+  ]
 };

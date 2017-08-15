b056a319375689c1f4a78f84d02a07a833587fa9
Jered Marshall <jmarsh410@gmail.com>
2017-08-15 14:37:29 -0500

    use cookies, new env variables, edit login check
    - for checking login status of users, we use cookieparser
    - make a function for checking the login status of users
    - pass login status in as a prop to <App />
    - prevent caching of redirects in production, since its super annoying while testing and changing around redirects
    - use new environment variable for port and environment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var cookieParser = require('cookie-parser');
 var React = require('react');
 var ReactDomServer = require('react-dom/server');
 var StaticRouter = require('react-router').StaticRouter;
@@ -13,24 +14,54 @@ var App = require('./dist-server/bundle.js').App.default;
 // serve static files from the static folder 
 app.use(express.static('dist-client/static'));
 
+// middleware for checking cookies
+app.use(cookieParser());
+
 // set the template engine to use nunjucks
 app.set('views', './views');
 nunjucks.configure('views', {express: app,autoescape: false});
 app.set('view engine', 'nunjucks');
 
+function checkLoginStatus(req, res){
+  // check the request for a cookie with a correct untapped id's
+  if (req.cookies.userToken) {
+    return true;
+  }
+  const urlHash = '#access_token';
+  // if no cookie, check the request url for a hash that looks like #access_token=clienttoken
+  if (req.url.indexOf(urlHash) > -1){
+    res.clearCookie('userToken');
+    // create userToken cookie
+    res.cookie('userToken', req.url.split(urlHash).pop(), {
+      maxAge: 60*60*24*7, // expires in one week
+    });
+    return true;
+  }
+  // the user is not logged in
+  return false;
+}
+
 function renderApp(req, res){
   var context = {};
+  var userIsLoggedIn = checkLoginStatus(req, res);
+  console.log('the user login status is: ' + userIsLoggedIn + '. and the path is ' + req.url);
   // get the contents of the index.html file
   var html = ReactDomServer.renderToString(
     <StaticRouter
       location = {req.url}
       context = {context}
     >
-      <App/>
+      <App userIsLoggedIn={userIsLoggedIn}/>
     </StaticRouter>
   );
+
   // render the App for the appropriate url path
-  if (context.url) {
+  if (context.url) { // HANDLE REDIRECTS
+    // on production, prevent caching so that redirects don't cache, because its super annoying
+    if (process.env.FH_ENV === 'development') {
+      console.log('app was redirected and not cached');
+      res.append('Cache-Control', 'no-cache, no-store, must-revalidate');
+    }
     res.writeHead(301, {
       Location: context.url
     })
@@ -42,6 +73,6 @@ function renderApp(req, res){
 
 app.get('/*', renderApp);
 
-app.listen(process.env.PORT || 5000, function(){
-  console.log('express app is listening on port ' + process.env.PORT || 5000);
+app.listen(process.env.FH_PORT || 5000, function(){
+  console.log('express app is listening on port ' + process.env.FH_PORT || 5000);
 });

32b820093807285a1fa14ee2d9aac88921766143
Jered Marshall <jmarsh410@gmail.com>
2017-08-15 20:04:17 -0500

    use the server side authUrl

--- src/components/login.js
+++ src/components/login.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import ButtonAnchor from './button-anchor';
 import '../styles/login.css';
 import pbuLogo from '../images/pbu_40_black.png';
-const authURL = 'https://untappd.com/oauth/authenticate/?client_id=B37286DA6E41C3C75634F4C0DB726E889052525C&response_type=token&redirect_url=http://festivalhopper.jeredmarshall.com/';
+const authURL = 'https://untappd.com/oauth/authenticate/?client_id=B37286DA6E41C3C75634F4C0DB726E889052525C&response_type=code&redirect_url=http://festivalhopper.jeredmarshall.com:5000';
 
 class Login extends Component {
   render(){

b2c9d3ec3e9bf36e59a6adac02e45eef08e8eab1
Jered Marshall <jmarsh410@gmail.com>
2017-10-15 14:31:42 -0500

    directory for holding json values that i can add to the curatedlists database collection

---
+++ _curated-lists/example-beer.js
@@ -0,0 +1,13 @@
+{
+  "id":1181221,
+  "name":"IPA",
+  "brewery":"Oskar Blues Brewery",
+  "image":"https://untappd.akamaized.net/site/beer_logos/beer-1181221_ee813_sm.jpeg",
+  "rating":0,
+  "isCheckedIn":false,
+  "isOpen":false,
+  "checked":false,
+  "isFavorite":false,
+  "bucket":0,
+  "index":0
+}

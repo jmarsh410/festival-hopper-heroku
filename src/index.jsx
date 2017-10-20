8a3d137fbc6d8d625a36991ec6e3626ea61a79cb
Jered Marshall <jmarsh410@gmail.com>
2017-10-20 08:48:17 -0500

    use new React hydrate feature. conditionally use render or hydrate method based on environment.

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, hydrate } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import utils from './utils/utils';
 import './styles/normalize.css';
 import './index.css';
 
-ReactDOM.render(
+const renderMethod = utils.isClientSide() ? render : hydrate;
+
+renderMethod(
   <BrowserRouter>
     <App />
   </BrowserRouter>,

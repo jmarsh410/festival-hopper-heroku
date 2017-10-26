a727318e6f8010a975241d1f9a81c65fa49453ed
Jered Marshall <jmarsh410@gmail.com>
2017-10-26 09:44:13 -0500

    give nav a state for opening and closing

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -5,18 +5,48 @@ import { NavLink } from 'react-router-dom';
 import '../styles/nav.css';
 
 class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+    this.iconClickHandler = this.iconClickHandler.bind(this);
+  }
+  iconClickHandler() {
+    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+    console.log(this.state);
+  }
   render() {
+    const navClasses = this.state.isOpen ? 'nav nav--open' : 'nav';
     return (
-      <nav className="nav">
-        <ul>
-          <li>
-            <NavLink to="/curated" activeClassName="selected" className="nav-curated">Curated Lists</NavLink>
-          </li>
-          <li>
-            <NavLink to="/brewery-search" activeClassName="selected" className="nav-breweries">Search Breweries</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <div className="navContainer">
+        <button className="hamburger" onClick={this.iconClickHandler}>Menu</button>
+        <nav id="nav" className={navClasses}>
+          <ul>
+            <li>
+              <NavLink
+                to="/curated"
+                activeClassName="selected"
+                className="nav-link nav-curated"
+              >Curated Lists</NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/brewery-search"
+                activeClassName="selected"
+                className="nav-link nav-breweries"
+              >Search Breweries</NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/logout"
+                activeClassName="selected"
+                className="nav-link nav-logout"
+              >Log out</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     );
   }
 }

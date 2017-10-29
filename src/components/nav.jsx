1655588c5fd5a62e15aaa7b1f1d481c450305901
Jered Marshall <jmarsh410@gmail.com>
2017-10-29 13:36:19 -0500

    removed login from the navigation. it will be moved to a settings area at later date

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -17,36 +17,25 @@ class Nav extends Component {
     console.log(this.state);
   }
   render() {
-    const navClasses = this.state.isOpen ? 'nav nav--open' : 'nav';
     return (
-      <div className="navContainer">
-        <button className="hamburger" onClick={this.iconClickHandler}>Menu</button>
-        <nav id="nav" className={navClasses}>
-          <ul>
-            <li>
-              <NavLink
-                to="/curated"
-                activeClassName="selected"
-                className="nav-link nav-curated"
-              >Curated Lists</NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/brewery-search"
-                activeClassName="selected"
-                className="nav-link nav-breweries"
-              >Search Breweries</NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/logout"
-                activeClassName="selected"
-                className="nav-link nav-logout"
-              >Log out</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <nav id="nav" className="nav">
+        <ul>
+          <li>
+            <NavLink
+              to="/curated"
+              activeClassName="selected"
+              className="nav-link nav-curated"
+            >Curated Lists</NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/brewery-search"
+              activeClassName="selected"
+              className="nav-link nav-breweries"
+            >Search Breweries</NavLink>
+          </li>
+        </ul>
+      </nav>
     );
   }
 }
